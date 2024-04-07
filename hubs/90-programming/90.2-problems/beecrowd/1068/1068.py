b1d0ba29185eb4aa7ffbd0e2e1c73c08024a1157
lpsm-dev-github <lpsm-dev@protonmail.com>
2024-04-07 12:39:13 -0300

    fix: rm var

--- hubs/90-programming/90.2-problems/beecrowd/1068/1068.py
+++ hubs/90-programming/90.2-problems/beecrowd/1068/1068.py
@@ -6,14 +6,13 @@ for element in range(0, 10000):
     values = [char for char in expression if char == '(' or char == ')']
     if values[0] == ')': print('incorrect')
     else:
-      count, correct = 0, True
+      count = 0
       for value in values:
         if value == '(':
           count += 1
         elif value == ')':
           count -= 1
         if count < 0:
-          correct = False
           break
       if count == 0:
         print('correct')
